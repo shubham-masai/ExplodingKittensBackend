b68bbba0a47c41d474233b5fa793760c8b91b8b5
Shubham Jayswal <shubhammasai03@gmail.com>
2024-03-16 16:19:56 +0530

    signup and login route created

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 const { connections } = require("./db");
+const { userRouter } = require("./routes/user.routes");
 app.use(express.json());
+app.use("/user", userRouter);
 app.listen(8080, async () => {
     try {
         await connections
---
+++ routes/user.routes.js
@@ -0,0 +1,69 @@
+const express = require("express");
+require('dotenv').config()
+const userRouter = express.Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const UserModel = require("../model/user.model");
+
+userRouter.post("/signup", async (req, res) => {
+    const { username, email, password } = req.body;
+    try {
+        let existing = await UserModel.findOne({ email });
+
+        if (existing) {
+            return res.status(400).send({ message: "Email Already Registered." });
+        }
+
+        bcrypt.hash(password, 5, async (err, hash) => {
+            if (err) {
+                return res.status(400).send({ message: err.message });
+            }
+            try {
+                let newUser = new UserModel({ username, email, password: hash });
+                await newUser.save();
+
+                let token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+                res.status(200).send({ message: "Registration successful.", token , username:newUser.username, score:newUser.score});
+            } catch (error) {
+                res.status(400).send({ message: error.message });
+            }
+        });
+    } catch (error) {
+
+        return res.status(400).send({ message: error.message });
+    }
+});
+
+
+userRouter.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await UserModel.findOne({ email });
+
+        if (!user) {
+            return res.status(404).send({ statusCode: 404, message: 'User not found' });
+        }
+        const isPasswordValid = await new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, result) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(result);
+                }
+            });
+        });
+
+        if (!isPasswordValid) {
+            return res.status(400).send({ message: "Invalid password" });
+        }
+        const token = jwt.sign({ username: user.username, userId: user._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+        res.status(200).json({ message: "Login successful.", token, username:user.username, score:user.score});
+
+    } catch (error) {
+        res.status(400).send({ message: error.message });
+    }
+});
+
+module.exports = {
+    userRouter
+}
